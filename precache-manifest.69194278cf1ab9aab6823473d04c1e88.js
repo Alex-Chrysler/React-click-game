4b19ff6337999e59d6c418bc84bbfcab49602590
achrysler <achrysler36@gmail.com>
2018-11-27 10:12:58 -0600

    Updates

---
+++ precache-manifest.69194278cf1ab9aab6823473d04c1e88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a2910cd085821ce393",
+    "url": "/React-click-game/static/js/runtime~main.83a2910c.js"
+  },
+  {
+    "revision": "3873f524427204b28314",
+    "url": "/React-click-game/static/js/main.3873f524.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/React-click-game/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "3873f524427204b28314",
+    "url": "/React-click-game/static/css/main.d571ac6c.chunk.css"
+  },
+  {
+    "revision": "86870d0c5754487b0bea6ea5a4d07895",
+    "url": "/React-click-game/index.html"
+  }
+];
